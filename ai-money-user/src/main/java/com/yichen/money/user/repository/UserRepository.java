64a1f46c663447e13f5f6e7f1d60ba33c927bb9b
wuchen <1721564595@qq.com>
2025-11-01 10:22:57 +0800

    feat：移除所有模块

--- ai-money-user/src/main/java/com/yichen/money/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yichen.money.user.repository;
-
-import com.yichen.money.user.model.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
-
-public interface UserRepository extends ReactiveCrudRepository<User, Long> {
-    Mono<User> findByUsername(String username);
-}
